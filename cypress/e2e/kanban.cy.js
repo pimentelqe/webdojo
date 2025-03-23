4ee474e0ff2bac2e1595ea3a1c519bd96196f5a9
pimentelqe <pimentelqe@gmail.com>
2025-03-23 20:59:32 -0300

    ajustes no readme

---
+++ cypress/e2e/kanban.cy.js
@@ -0,0 +1,24 @@
+describe('Kanban board', () => {
+    it('deve mover uma tarefa de todo para Done e atualizar o board', () => {
+        cy.start()
+        cy.submitLoginForm('papito@webdojo.com', 'katana123')
+        cy.contains('Kanban').click()
+
+        const dataTransfer = new DataTransfer()
+
+        cy.contains('div[draggable=true]', 'Documentar API')
+            .trigger('dragstart', { dataTransfer })
+
+        cy.get('.column-done')
+            .trigger('drop', { dataTransfer })
+            .find('h3')
+            .should('have.text', 'Done (4)')
+
+         cy.get('.column-done')   
+            .should('include.text', 'Documentar API')
+            .and('include.text', 'Criar documentação da API com Swagger')
+
+
+
+    })
+})
