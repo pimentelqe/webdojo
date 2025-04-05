9746e58b1dd1a083136128e0736bc7b84f96e366
pimentelqe <pimentelqe@gmail.com>
2025-04-05 17:17:18 -0300

    validando registros em tabelas

--- cypress/e2e/github.cy.js
+++ cypress/e2e/github.cy.js
@@ -13,5 +13,19 @@ describe('Gerenciamento de perfis no Github', () => {
 
         cy.contains('button', 'Adicionar Perfil').click()
 
+        cy.contains('table tbody tr', 'ferpioli')
+        .should('be.visible')
+        .as('trProfile')
+
+        cy.get('@trProfile')
+        .contains('Fernando Pimentel')
+        .should('be.visible')
+
+       cy.get('@trProfile')
+        .contains('QA')
+        .should('be.visible')
+
+
     })
+
 })
